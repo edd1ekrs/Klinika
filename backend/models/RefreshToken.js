98fa4a3369361a8df0345231835346c70de94ec9
edd1ekrs <edi04.ek@gmail.com>
2026-03-04 21:29:59 +0100

    Rikrijimi i folderit pas disa gabimeve

--- backend/models/RefreshToken.js
+++
@@ -1,7 +0,0 @@
-const { DataTypes } = require('sequelize');
-module.exports = (sequelize) => sequelize.define('RefreshToken', {
-  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  userId: { type: DataTypes.UUID, allowNull: false },
-  token: { type: DataTypes.STRING(500), allowNull: false },
-  expiresAt: { type: DataTypes.DATE, allowNull: false }
-}, { tableName: 'refresh_tokens', timestamps: true });
